b4b6ede6062b5e00847dd239d4d236452a98a190
MassimoNonBossetti <massimo.turrini@sutdenti.isissgobetti.it>
2025-10-22 09:03:34 +0200

    test1

---
+++ ProvaTpsit/ProvaTpsit/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProvaTpsit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+    class veicolo
+    {
+        public string marca;
+    }
+    class auto:veicolo
+    {
+
+    }
+}
